229c3b268115263242c916dd3906f401bccbd8df
Deepak Sehrawat <parkashsehrawat2000@gmail.com>
2021-06-04 21:20:41 +0530

    Testing unknown error

--- middleware/error.js
+++ middleware/error.js
@@ -2,7 +2,8 @@ const logger = require("./logger");
 
 function error(err, req, res, next) {
   logger.error(err.message);
-  res.status(500).send("Something failed while parsing");
+  // res.status(500).send("Something failed while parsing");
+  res.status(500).send(err);
 }
 
 module.exports = error;
