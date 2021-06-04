b720cdea525fe447de7e452f12c8578bf5e64fea
Deepak Sehrawat <parkashsehrawat2000@gmail.com>
2021-06-04 22:56:50 +0530

    Testing 5

--- middleware/error.js
+++ middleware/error.js
@@ -2,8 +2,7 @@ const logger = require("./logger");
 
 function error(err, req, res, next) {
   logger.error(err.message);
-  // res.status(500).send("Something failed while parsing");
-  res.status(500).send(err);
+  res.status(500).send("Something failed while parsing");
 }
 
 module.exports = error;
